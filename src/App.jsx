b55ed23ab6a1163285ec570cbf18e0744db673c0
KhrAini <ayni110603@gmail.com>
2024-11-27 16:42:21 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     return(
     <Router>
         <Routes>
-            <Route path="/" element={<LandingPage />} />
+            <Route path="/" element={<LandingPage/>} />
         </Routes>
     </Router>
     );
