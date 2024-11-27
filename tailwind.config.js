af261ee039c435c2f8649f43145381700fe36a04
arufaki <fakihkazorla@gmail.com>
2024-11-27 07:51:55 +0700

    chore(tailwind): adds nunito font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        fontFamily: {},
+        fontFamily: {
+            nunito: ["Nunito", "sans-serif"],
+        },
         extend: {},
     },
     plugins: [],
