09d3a9349d853000d00a5ac47bee6195e1987bd4
KhrAini <ayni110603@gmail.com>
2024-11-28 22:58:37 +0700

    up footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,15 @@ import Instagram from "../assets/Instagram.png";
 
 const Footer = () => {
   return (
-    <div className=" fixed h-[670px] sm:h-[400px] w-full bg-[#2E7D32] flex flex-col items-center">
-      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between w-full px-[24px] sm:px-[80px] py-[40px] sm:py-[110px] gap-8 sm:gap-0">
-        {/* Kolom pertama */}
-        <div className="flex flex-col items-start gap-4 w-full sm:w-auto">
-          <img src={Logo} alt="EcoMate Logo" className="w-[48px] h-[48px]" />
-          <p className="text-white text-[26px] font-bold">EcoMate</p>
-          <p className="text-white text-sm text-left text-[18px]">
+    <div className=" fixed h-full sm:h-[400px] w-full bg-[#2E7D32] flex flex-col items-center justify-center">
+      <div className="flex flex-col sm:flex-row  w-full px-[24px] sm:px-[80px] py-[40px] sm:py-[110px] sm:gap[0px] gap-[48px]">
+        <div className="flex flex-col items-start gap-[16px] h-[180px] w-[278px]  sm:pl-[0px] pl-[48px] ">
+          <div className=" flex flex-row gap-[12px]">
+              <img src={Logo} alt="EcoMate Logo" className="w-[48px] h-[48px]" />
+              <h5 className="text-white text-[26px] font-bold">EcoMate</h5>          
+          </div>
+
+          <p className="text-white text-left text-[18px] pr-[16px] ">
             Jl. Ahmad Dahlan no.23, Kebumen, Jawa Tengah 54311
           </p>
           <div className="flex gap-4">
@@ -34,40 +36,36 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="flex flex-row gap-8 w-full sm:w-auto">
-          {/* Kategori */}
-          <div className="flex flex-col items-start text-left">
+        <div className="flex flex-cop items-start pl-[48px] sm:flex-row gap-[40px] sm:gap-[62px] w-full sm:w-auto sm:pl-[364px] pl-[0px] ">
+          <div className="flex flex-col  items-start text-left gap-[12px]">
             <h2 className="text-white text-lg font-semibold text-[24px]">Kategori</h2>
-            <div className="flex flex-col mt-2">
-              <p className="text-white text-sm mt-2 text-[18px]">Baju</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Sepatu</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Sandal</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Perabotan</p>
+            <div className="flex flex-col gap-[12px] ">
+              <p className="text-white text-[18px]">Baju</p>
+              <p className="text-white text-[18px]">Sepatu</p>
+              <p className="text-white text-[18px]">Sandal</p>
+              <p className="text-white text-[18px]">Perabotan</p>
             </div>
           </div>
 
-          {/* Tantangan */}
-          <div className="flex flex-col items-start text-left">
-            <h2 className="text-white text-lg font-semibold text-[24px]">Tantangan</h2>
-            <div className="flex flex-col mt-2">
-              <p className="text-white text-sm mt-2 text-[18px]">Energy Saver</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Plastic Free</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Green Communate</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Tree Challenge</p>
-            </div>
-          </div>
-
-          {/* Ecomate */}
-          <div className="flex flex-col items-start text-left">
-            <h2 className="text-white text-lg font-semibold text-[24px]">Ecomate</h2>
-            <div className="flex flex-col mt-2">
-              <p className="text-white text-sm mt-2 text-[18px]">Beranda</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Tentang</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Belanja</p>
-              <p className="text-white text-sm mt-2 text-[18px]">Tantangan</p>
+          <div className="flex flex-col items-start text-left gap-[12px] px-[61px]">
+            <h2 className="text-white text-lg font-semibold text-[24px]  ">Tantangan</h2>
+            <div className="flex flex-col gap-[12px] ">
+              <p className="text-white text-[18px]">Energy Saver</p>
+              <p className="text-white text-[18px]">Plastic Free</p>
+              <p className="text-white text-[18px]">Green Communate</p>
+              <p className="text-white text-[18px]">Tree Challenge</p>
             </div>
           </div>
         </div>
+            <div className="flex flex-col text-left gap-[12px] pr-[292px] sm:pr-[0px] pl-[48px] pb-[48px] sm:pb-[0px] ">
+              <h2 className="text-white text-lg font-semibold text-[24px]">Ecomate</h2>
+              <div className="flex flex-col gap-[12px] ">
+              <p className="text-white text-[18px]">Beranda</p>
+              <p className="text-white text-[18px]">Tentang</p>
+              <p className="text-white text-[18px]">Belanja</p>
+              <p className="text-white text-[18px]">Tantangan</p>
+            </div>
+          </div>
       </div>
     </div>
   );
