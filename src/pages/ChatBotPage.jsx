d1a19f28fe730a2215b893d375cc64ed6b166c26
arufaki <fakihkazorla@gmail.com>
2024-12-01 20:06:00 +0700

    feat(chatbot): add navbar to chatbot page

--- src/pages/ChatBotPage.jsx
+++ src/pages/ChatBotPage.jsx
@@ -1,5 +1,12 @@
+import Navbar from "../components/Navbar";
+
 const ChatBotPage = () => {
-    return <div>ChatbotPage</div>;
+    return (
+        <div className="flex flex-col min-h-screen bg-secondary">
+            <Navbar />
+            
+        </div>
+    );
 };
 
 export default ChatBotPage;
