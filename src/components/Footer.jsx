79e80aed6c0633113973da209bd13cdc961e6705
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-01 22:44:18 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Footer = () => {
   return (
 
-    <div className=" bg-primary  ">
+    <div className=" bg-primary ">
       <div className="flex flex-col sm:flex-row  md:py-[110px] py-10 items-center justify-center ">
         <div className="flex flex-col items-start gap-[16px] h-[180px] w-[400px]  sm:pl-[0px] pl-10 ">
           <div className=" flex flex-row gap-[12px]">
@@ -56,7 +56,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-            <div className="flex flex-col text-left gap-[12px] pr-[292px] sm:pr-[0px] pl-12 pb-12 sm:pb-[0px] ">
+            <div className="flex flex-col text-left gap-[12px]  sm:pr-[0px] pl-12 pb-12 sm:pb-[0px] ">
               <h2 className="text-white text-lg font-semibold text-[24px]">Ecomate</h2>
               <div className="flex flex-col gap-[12px] ">
               <a href="#" className="text-white text-[18px] hover:text-gray-200">Beranda</a>
