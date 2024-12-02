6f5a9eb33c30ee5324de705aa1ebf24e452ab9fc
arufaki <fakihkazorla@gmail.com>
2024-12-02 17:10:21 +0700

    chore(api): use defined api url variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import useAuthStore from "../stores/useAuthStore";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: apiUrl,
     headers: { "Content-Type": "application/json" },
 });
 
