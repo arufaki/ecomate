e7ff8b28ab7c742ec2a6c0775036d346f3280e75
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-05 16:11:38 +0700

    feat: fix swiper landing page

--- src/components/LandingPage/Product.jsx
+++ src/components/LandingPage/Product.jsx
@@ -8,17 +8,20 @@ import { truncateContent } from "../../hooks/useTruncates";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { div } from 'motion/react-client';
 // Sample product data
 
 
     const ProductCarousel = () => {
         const [products, setProducts] = useState([]);
-
+        const [isLoading, setIsLoading] = useState(true);
         useEffect(() => {
             const fetchProducts = async () => {
                 try {
+                    setIsLoading(true);
                     const response = await api.get('/products');
                     setProducts(response.data.data);
+                    setIsLoading(false);
                 } catch (error) {
                     console.error('Error fetching products:', error);
                 }
@@ -33,43 +36,52 @@ import 'swiper/css/pagination';
                 <h1 className="md:text-5xl text-xl text-neutral-800 text-center justify-center font-bold">Pilihan Anda Membuat Perbedaan</h1>
             </div>
             {/* Slider */}
-            <div className="relative w-[80%] md:w-[80%] mx-auto ">
-            <Swiper
-                modules={[ Mousewheel]}
-                spaceBetween={20}
-                slidesPerView={1}
-                loop={true}
-                mousewheel={true}
-                breakpoints={{
-                640: {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
-                },
-                1024: {
-                    slidesPerView: 3.5,
-                    spaceBetween: 30,
-                },
-                }}
-                grabCursor={true}
-                className="pb-12 md:h-[500px] h-[350px]"
-            >
-                {products.slice(6, 12).map((product) => (
-                <SwiperSlide key={product.product_id} className="px-4">
+            
+                {isLoading ? (
+                    <div className="flex justify-center items-center min-h-[500px]">
+                        <div className="animate-spin rounded-full h-20 w-20 border-b-[3px] border-t-[3px] border-primary"></div>
+                    </div>
+                ):
+                (
+                    <div className="relative w-[80%] md:w-[80%] mx-auto ">
+                        <Swiper
+                            modules={[ Mousewheel]}
+                            spaceBetween={20}
+                            slidesPerView={1}
+                            loop={true}
+                            mousewheel={true}
+                            breakpoints={{
+                            640: {
+                                slidesPerView: 2,
+                                spaceBetween: 20,
+                            },
+                            1024: {
+                                slidesPerView: 3.5,
+                                spaceBetween: 30,
+                            },
+                            }}
+                            grabCursor={true}
+                            className="pb-12 md:h-[500px] h-[350px]"
+                        >
+                        {products.slice(6, 12).map((product) => (
+                        <SwiperSlide key={product.product_id} className="px-4">
 
-                            <Card
-                                image={product.images[0]?.image_url || '/default-product.png'}
-                                name={product.name}
-                                description={truncateContent(product.description, 100)}
-                                price={(product.price).toLocaleString("id-ID")}
-                                link={`/detail-produk/${product.product_id}`}
-                            />
+                                    <Card
+                                        image={product.images[0]?.image_url || '/default-product.png'}
+                                        name={product.name}
+                                        description={truncateContent(product.description, 100)}
+                                        price={(product.price).toLocaleString("id-ID")}
+                                        link={`/detail-produk/${product.product_id}`}
+                                    />
 
-                </SwiperSlide>
-                ))}
-            </Swiper>
+                        </SwiperSlide>
+                        ))}
+                        </Swiper>
+                    </div>   
+                )}
+                
+            
         
-
-            </div>
 
             {/* End Slider */}
             </div>
