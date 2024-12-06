021867fa9b7b5fc232cc67f14e446ebc5f9d456a
arufaki <fakihkazorla@gmail.com>
2024-12-07 04:10:21 +0700

    feat(admin): add admin product page route

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import PaymentPage from "./pages/PaymentPage";
 import DayChallengePage from "./pages/DayChallengePage";
 import Dashboard from "./pages/AdminPages/Dashboard";
+import Products from "./pages/AdminPages/Products";
 const App = () => {
     return (
         <Router>
@@ -29,11 +30,13 @@ const App = () => {
                 <Route path="/forgot-password" element={<ForgotPasswordPage />} />
 
                 {/* Guest routes (untuk login dan register, hanya bisa diakses oleh user yang belum login) */}
-                <Route element={<GuestRoute />}>
+                <Route element={<GuestRoute redirectPath="/" />}>
                     {/* End User Route */}
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
+                </Route>
 
+                <Route element={<GuestRoute redirectPath="/admin/dashboard" />}>
                     {/* Admin Route protected */}
                     <Route path="/login-admin" element={<AdminLoginPage />} />
                 </Route>
@@ -47,8 +50,10 @@ const App = () => {
 
                     {/* Admin Route */}
                     {/* email admin: admin2@ecomate.store pass : admin2 */}
+
                     <Route path="/add-product" element={<AddProductPage />} />
                     <Route path="/admin/dashboard" element={<Dashboard />} />
+                    <Route path="/admin/produk" element={<Products />} />
                 </Route>
             </Routes>
         </Router>
