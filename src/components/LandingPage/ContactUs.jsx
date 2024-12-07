180967211a08f3d93f2630bf4df889bd963d0a21
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-07 20:54:28 +0700

    update imported image

--- src/components/LandingPage/ContactUs.jsx
+++ src/components/LandingPage/ContactUs.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-
-
+import Women from "../../assets/png/women.png";
+import Map from "../../assets/png/map.png";
+import Envelope from "../../assets/png/envelope.png";
+import Phone from "../../assets/png/phone.png";
 const ContactUs = () => {
     return (
         <div className="flex flex-col justify-center items-center bg-secondary text-neutral-800">
@@ -8,13 +10,13 @@ const ContactUs = () => {
                 <h1 className="font-bold sm:text-[48px] text-[20px] text-center pt-[24px] sm:pt-[80px] ">Hubungi Kami</h1>
                 <div className="flex flex-col bg-red sm:flex-row items-center justify:center sm:justify-space-between px-[24px] sm:px-[80px] pt-[24px] sm:pt-[72px] sm:gap-[121px] gap-[40px] ">
                     <div>
-                        <img src="../src/assets/png/women.png" alt="women" className="sm:h-[593px] sm:w-[621px] h-[296px] w-[382px] rounded-[50px] "/>
+                        <img src={Women} alt="women" className="sm:h-[593px] sm:w-[621px] h-[296px] w-[382px] rounded-[50px] "/>
                     </div>
                     <div className="pt-[0px] sm:pt-[57px] sm:pb-[57px] ">
                         <div className="sm:pb-[137px] pb-[24px] ">
                             <p className="sm:text-[36px] text-[20px] sm:text-4xl font-bold sm:pb-[36px] pb-[24px] ">Alamat Kami</p>
                             <div className="flex flex-row gap-[12px] ">
-                                <img src="../src/assets/png/map.png" alt="Map Icon" className="h-[46px] w-[46px]  " />
+                                <img src={Map} alt="Map Icon" className="h-[46px] w-[46px]  " />
                                 <div>
                                     <p className="text-[16px] sm:text-[24px] ">Indonesia</p>
                                     <p className="text-[14px] sm:text-[24px] " >Rungkut Kidul VI No 8, Rungkut Kidul,<br /> Rungkut, Surabaya</p>
@@ -25,14 +27,14 @@ const ContactUs = () => {
                             <p className="text-[20px] sm:text-[36px] text-4xl font-bold sm:pb-[36px] pb-[24px] ">Kontak Kami</p>
                             <div className="flex sm:flex-row flex-col sm:gap-[56px] gap-[24px] ">
                                     <div className="flex flex-row gap-[12px] ">
-                                    <img src="../src/assets/png/envelope.png" alt="Envelope" className="h-[46px] w-[46px] " />
+                                    <img src={Envelope} alt="Envelope" className="h-[46px] w-[46px] " />
                                     <div>
                                         <p className="text-[16px] sm:text-[24px]">Email</p>
                                         <p className="text-[14px] sm:text-[24px] ">example@mail.com</p>
                                     </div>
                                 </div>
                                 <div className="flex flex-row gap-[12px] ">
-                                    <img src="../src/assets/png/phone.png" alt="Phone" className="h-[46px] w-[46px] " />
+                                    <img src={Phone} alt="Phone" className="h-[46px] w-[46px] " />
                                     <div>
                                         <p className="text-[16px] sm:text-[24px]">Telepon</p>
                                         <p className="text-[14px] sm:text-[24px]">+62 85745104969</p>
