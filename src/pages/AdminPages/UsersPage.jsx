8421da89c1aacba4616f4ff3066a4df96fe56dc5
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-07 16:18:05 +0700

    import api

--- src/pages/AdminPages/UsersPage.jsx
+++ src/pages/AdminPages/UsersPage.jsx
@@ -2,6 +2,15 @@ import React,{ useState , useEffect} from "react";
 import useSideBarStore from "../../stores/useSideBarStore";
 import Sidebar from "../../components/Admin/Sidebar";
 import Header from "../../components/Admin/Header";
+import api from "../../services/api";
+import eye from "../../assets/svg/admin-icon/eye.svg";
+import pencil from "../../assets/svg/admin-icon/pencil.svg";
+import trash from "../../assets/svg/admin-icon/trash.svg";
+import arrowUpDown from "../../assets/svg/admin-icon/arrows-up-down.svg";
+import arrow from "../../assets/svg/admin-icon/arrow-right.svg";
+import { Toast } from "../../utils/function/toast";
+import { Plus, Search } from "lucide-react";
+import { Link } from "react-router";
 const Users = () => {
     const { isOpen: sidebarOpen } = useSideBarStore();
     
