dc63360751041e0386f869174017ae868ff9e210
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-12 00:08:08 +0700

    Update ForumPage.jsx

--- src/pages/ForumPage.jsx
+++ src/pages/ForumPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroForum from "../components/ForumPage/HeroForum";
@@ -7,11 +7,32 @@ import useAuthStore from "../stores/useAuthStore";
 import StickyCtaButton from "../components/StickyCtaButton";
 import { useNavigate } from "react-router";
 import { Toast } from "../utils/function/toast";
+import api from "../services/api";
 const ForumPage = () => {
   const { token } = useAuthStore();
-
+  
   const navigate = useNavigate();
-
+  const [forums, setForums] = useState([]);
+  const [posted, setPosted] = useState(true);
+  
+  const handleOnPosted = (onPosted) => {
+    setPosted(onPosted);
+  }
+  const getForum = async () => {
+    try {
+      const response = await api.get("/forums");
+      setForums(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+    useEffect(() => {
+      if(posted){
+        getForum();
+        setPosted(false);
+      }
+      
+    }, [posted]);
   useEffect(() => {
     if (!token) {
       Toast.fire({
@@ -25,8 +46,8 @@ const ForumPage = () => {
     <div className="bg-secondary">
       <Navbar active="forum" />
       <div className="min-h-screen">
-        <HeroForum />
-        <ForumPost />        
+        <HeroForum onPosted={handleOnPosted}/>
+        <ForumPost forums={forums}/>        
       </div>
       <Footer />
       <StickyCtaButton />
