f90b6f503673553b8815ab807c58aae93b1b3be2
arufaki <fakihkazorla@gmail.com>
2024-12-12 15:09:05 +0700

    fix(profile): add loading state & phone validation

--- src/components/ProfilePage/Navigasi/Address.jsx
+++ src/components/ProfilePage/Navigasi/Address.jsx
@@ -1,66 +1,81 @@
+import React, { useState, useEffect } from "react";
+import api from "../../../services/api";
+import { Toast } from "../../../utils/function/toast";
+const AlamatContent = ({ Data }) => {
+    const [loading, setLoading] = useState(false);
 
-import React,{ useState, useEffect} from "react";
- const AlamatContent = ({Data}) => {
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        address: '',
-        gender: 'male', // Default nilai
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+        gender: "male", // Default nilai
     });
     useEffect(() => {
         if (Data) {
             setFormData({
-                name: Data.name || '',
-                email: Data.email || '',
-                phone: Data.phone || '',
-                address: Data.address || '',
-                gender: Data.gender === 'male' ? 'male' : 'female',
+                name: Data.name || "",
+                phone: Data.phone || "",
+                address: Data.address || "",
+                gender: Data.gender === "male" ? "male" : "female",
             });
         }
     }, [Data]);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setFormData(prevState => ({
+        setFormData((prevState) => ({
             ...prevState,
-            [name]: value
+            [name]: value,
         }));
     };
 
-    const handleUpdate = async (event) => {
-        event.preventDefault();
+    const handleUpdate = async (e) => {
+        e.preventDefault();
         try {
+            setLoading(true);
             const response = await api.put(`/users/update`, formData);
-            console.log(response.data);
+            if (response.status === 200) {
+                Toast.fire({
+                    icon: "success",
+                    title: "Sukses Update Alamat",
+                });
+            } else {
+                throw error;
+            }
         } catch (error) {
+            Toast.fire({
+                icon: "error",
+                title: `Ubah Password Gagal!`,
+            });
             console.error(error);
+        } finally {
+            setLoading(false);
         }
     };
     return (
-     <div className="p-6">
-        <h2 className="text-2xl font-bold mb-4">Alamat Saya</h2>
-        <div className="space-y-4">
-            <form onSubmit={handleUpdate}>
-            <div>
-                <label className="block text-sm font-medium text-gray-700">Alamat Lengkap</label>
-                <hr />
-                <textarea 
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50" 
-                placeholder="Masukkan alamat lengkap"
-                rows={4}
-                name="address"
-                onChange={handleInputChange}
-                defaultValue={formData.address}
-                ></textarea>
+        <div className="p-6">
+            <h2 className="text-2xl font-bold mb-4">Alamat Saya</h2>
+            <div className="space-y-4">
+                <form onSubmit={handleUpdate}>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">Alamat Lengkap</label>
+                        <hr />
+                        <textarea
+                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+                            placeholder="Masukkan alamat lengkap"
+                            rows={4}
+                            name="address"
+                            onChange={handleInputChange}
+                            defaultValue={formData.address}
+                        ></textarea>
+                    </div>
+                    <button className="bg-primary py-3 px-4 block w-[103px] rounded-lg text-white mt-4" disabled={loading}>
+                        {loading ? <span className="loading loading-spinner text-success"></span> : "Simpan"}
+                    </button>
+                </form>
             </div>
-            <button className='bg-primary py-3 px-4 block w-[103px] rounded-lg text-white'>
-                    Simpan
-            </button>
-            </form>
         </div>
-    </div>
-
     );
 };
 export default AlamatContent;
--- src/components/ProfilePage/Navigasi/ProfilContent.jsx
+++ src/components/ProfilePage/Navigasi/ProfilContent.jsx
@@ -1,147 +1,150 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import api from "../../../services/api";
 import { Toast } from "../../../utils/function/toast";
 const ProfilContent = ({ Data }) => {
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        address: '',
-        gender: 'male', // Default nilai
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+        gender: "male", // Default nilai
     });
     useEffect(() => {
         if (Data) {
             setFormData({
-                name: Data.name || '',
-                email: Data.email || '',
-                phone: Data.phone || '',
-                address: Data.address || '',
-                gender: Data.gender === 'male' ? 'male' : 'female',
+                name: Data.name || "",
+                phone: Data.phone || "",
+                address: Data.address || "",
+                gender: Data.gender === "male" ? "male" : "female",
             });
         }
     }, [Data]);
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setFormData(prevState => ({
+
+        if (name === "phone") {
+            // Validasi nomor telepon
+            if (value && (!value.startsWith("08") || value.length !== 12)) {
+                setError("Nomor telepon harus dimulai dengan '08' dan berisi 12 digit.");
+            } else {
+                setError(""); // Bersihkan error jika valid
+            }
+        }
+
+        setFormData((prevState) => ({
             ...prevState,
-            [name]: value
+            [name]: value,
         }));
     };
 
-    const handleUpdate = async (event) => {
-        event.preventDefault();
-        
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+        console.log(formData);
         try {
-            console.log(formData);
-            const response = await api.put(`/users/update`, formData);
-            
+            setLoading(true);
+            await api.put(`/users/update`, formData);
             Toast.fire({
                 icon: "success",
                 title: "Sukses memperbarui data",
-            })
+            });
         } catch (error) {
             Toast.fire({
                 icon: "error",
                 title: "Gagal memperbarui data",
-            })
+            });
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
-    <div className="p-6">
-        <div className="space-y-4">
-            <form onSubmit={handleUpdate}>
-            <div className='flex flex-row mt-2'>
-                <label className="text-xl w-40 py-3 items-center">User Name</label>
-                <input 
-                type="text" 
-                className="max-w-[484px] py-3 px-4 block w-full border-gray-200 rounded-lg text-xl" 
-                defaultValue={Data.username}
-                disabled={true}
-                />
-            </div>
-            <div className='flex flex-row mt-2'>
-                <label className="text-xl w-40 py-3 items-center">Nama</label>
-                <input 
-                type="text" 
-                name="name"
-                className="max-w-[484px] py-3 px-4 block w-full border-gray-200 rounded-lg text-xl" 
-                placeholder="Masukkan nama lengkap"
-                defaultValue={Data.name}
-                onChange={handleInputChange}
-                />
-            </div>
-            <div className='flex flex-row mt-2'>
-                <label className="text-xl w-40 py-3">Email</label>
-                <input 
-                type="email" 
-                name="email"
-                className="max-w-[484px] py-3 px-4 block w-full border-gray-200 rounded-lg text-xl" 
-                placeholder="Masukkan email"
-                defaultValue={Data.email}
-                onChange={handleInputChange}
-                />
-            </div>
-            <div className='flex flex-row mt-2'>
-                <label className="text-xl w-40 py-3">No telpon</label>
-                <input 
-                type="number" 
-                name="phone"
-                className="max-w-[484px] py-3 px-4 block w-full border-gray-200 rounded-lg text-xl" 
-                placeholder="Masukkan nomer"
-                defaultValue={Data.phone}
-                onChange={handleInputChange}
-                />
-            </div>
-            <div className="flex flex-row items-center space-x-4">
-                <label className="text-xl w-40 py-3">Jenis Kelamin</label>
-                <div className="flex items-center space-x-3">
-                    <div className="flex">
-                        <input
-                            type="radio"
-                            name="gender"
-                            id="male"
-                            value="male"
-                            checked={formData.gender === 'male'}
-                            onChange={handleInputChange}
-                            className="shrink-0 mt-0.5 border-gray-200 rounded-full text-green-600 focus:ring-green-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
-                        />
-                        <label 
-                            htmlFor="male" 
-                            className="text-md text-gray-500 ml-2 dark:text-gray-400"
-                        >
-                            Laki-laki
-                        </label>
+        <div className="p-6">
+            <div className="space-y-4">
+                <form onSubmit={handleUpdate}>
+                    <div className="flex flex-row mt-2">
+                        <label className="text-xl w-40 py-3 items-center">User Name</label>
+                        <input type="text" className="max-w-[484px] py-3 px-4 block w-full border border-[#BBBBBB] rounded-lg text-xl" defaultValue={Data.username} disabled={true} />
                     </div>
-                    <div className="flex">
+                    <div className="flex flex-row mt-2">
+                        <label className="text-xl w-40 py-3 items-center">Nama</label>
                         <input
-                            type="radio"
-                            name="gender"
-                            id="female"
-                            value="female"
-                            checked={formData.gender === 'female'}
+                            type="text"
+                            name="name"
+                            className="max-w-[484px] py-3 px-4 block w-full border border-[#BBBBBB] rounded-lg text-xl"
+                            placeholder="Masukkan nama lengkap"
+                            defaultValue={Data.name}
                             onChange={handleInputChange}
-                            className="shrink-0 mt-0.5 border-gray-200 rounded-full text-green-600 focus:ring-green-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
                         />
-                        <label 
-                            htmlFor="female" 
-                            className="text-md text-gray-500 ml-2 dark:text-gray-400"
-                        >
-                            Perempuan
-                        </label>
                     </div>
-                </div>
+                    <div className="flex flex-row mt-2">
+                        <label className="text-xl w-40 py-3">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            className="max-w-[484px] py-3 px-4 block w-full border border-[#BBBBBB] rounded-lg text-xl"
+                            placeholder="Masukkan email"
+                            defaultValue={Data.email}
+                            onChange={handleInputChange}
+                            disabled
+                        />
+                    </div>
+                    <div className="flex flex-row mt-2">
+                        <label className="text-xl w-40 py-3">No telpon</label>
+                        <input
+                            type="number"
+                            name="phone"
+                            className="max-w-[484px] py-3 px-4 block w-full border border-[#BBBBBB] rounded-lg text-xl"
+                            placeholder="Masukkan nomer"
+                            defaultValue={Data.phone}
+                            minLength={12}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+                    <div className="flex flex-row items-center space-x-4">
+                        <label className="text-xl w-40 py-3">Jenis Kelamin</label>
+                        <div className="flex items-center space-x-3">
+                            <div className="flex">
+                                <input
+                                    type="radio"
+                                    name="gender"
+                                    id="male"
+                                    value="male"
+                                    checked={formData.gender === "male"}
+                                    onChange={handleInputChange}
+                                    className="shrink-0 mt-0.5 border-gray-200 rounded-full text-green-600 focus:ring-green-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
+                                />
+                                <label htmlFor="male" className="text-md text-gray-500 ml-2 dark:text-gray-400">
+                                    Laki-laki
+                                </label>
+                            </div>
+                            <div className="flex">
+                                <input
+                                    type="radio"
+                                    name="gender"
+                                    id="female"
+                                    value="female"
+                                    checked={formData.gender === "female"}
+                                    onChange={handleInputChange}
+                                    className="shrink-0 mt-0.5 border-gray-200 rounded-full text-green-600 focus:ring-green-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-green-500 dark:checked:border-green-500 dark:focus:ring-offset-gray-800"
+                                />
+                                <label htmlFor="female" className="text-md text-gray-500 ml-2 dark:text-gray-400">
+                                    Perempuan
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" className="bg-primary py-3 px-4 block w-[103px] rounded-lg text-white" disabled={loading}>
+                        {loading ? <span className="loading loading-spinner text-success"></span> : "Simpan"}
+                    </button>
+                </form>
             </div>
-            <button 
-                type="submit"
-                className='bg-primary py-3 px-4 block w-[103px] rounded-lg text-white'
-            >
-                Simpan
-            </button>
-            </form>
         </div>
-    </div>
-    )
+    );
 };
 
 export default ProfilContent;
