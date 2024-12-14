38ddd238d6c0c750fdc9e2597c3caab41cd8d73f
arufaki <fakihkazorla@gmail.com>
2024-12-15 02:35:52 +0700

    fix(chatbot): updates image import path and user welcome message

--- src/components/ChatbotPage/InputPrompt.jsx
+++ src/components/ChatbotPage/InputPrompt.jsx
@@ -1,3 +1,5 @@
+import sendButton from "../../assets/svg/send.svg";
+
 const InputPrompt = ({ onSubmit, register }) => {
     return (
         <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 w-full max-w-[968px] px-4">
@@ -8,12 +10,9 @@ const InputPrompt = ({ onSubmit, register }) => {
                     className="input input-bordered input-md w-full font-normal bg-[#f9f9eb] max-[500px]:text-sm text-xl shadow-lg rounded-lg px-10 py-5 text-[#1F2937]"
                     {...register("message")}
                 />
-                <div className="absolute inset-y-4 start-0 flex items-center ps-4 cursor-pointer">
-                    <img src="../src/assets/svg/link.svg" alt="icon-start" />
-                </div>
                 <button>
                     <div className="absolute inset-y-6 right-3 transform -translate-y-1/2 flex items-center cursor-pointer">
-                        <img src="../src/assets/svg/send.svg" alt="icon-end" />
+                        <img src={sendButton} alt="icon-end" />
                     </div>
                 </button>
             </form>
--- src/components/ChatbotPage/WelcomeSection.jsx
+++ src/components/ChatbotPage/WelcomeSection.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import useUserStore, { loadUserData } from "../../stores/useUserStore";
+
 const WelcomeSection = ({ handleQuestionClick }) => {
+    const user = useUserStore((state) => state.user);
+
+    useEffect(() => {
+        loadUserData("/users/profile");
+    }, []);
+
     return (
         <div className="flex flex-col items-center justify-center w-full pt-20 max-[425px]:pt-10" style={{ scrollbarWidth: "none" }}>
             <div className="text-center mb-6 px-3">
-                <h1 className="font-bold text-[30px] text-black max-[500px]:text-2xl">Halo, Resiana!</h1>
+                <h1 className="font-bold text-[30px] text-black max-[500px]:text-2xl">Halo, {user.name}!</h1>
                 <h2 className="font-bold text-[30px] text-black max-[500px]:text-2xl">Apa yang bisa saya bantu?</h2>
             </div>
             <div className="flex justify-center flex-wrap gap-4 pt-6 max-w-[720px]">
