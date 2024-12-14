00836e433277f7777028a2e855a3643b66a90a3f
arufaki <fakihkazorla@gmail.com>
2024-12-15 00:39:36 +0700

    style(profile): updates import paths and removes unnecessary code

--- src/components/ProfilePage/Navigasi/DeleteContent.jsx
+++ src/components/ProfilePage/Navigasi/DeleteContent.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import api from "../../../services/api";
 import { Toast } from "../../../utils/function/toast";
 import useAuthStore from "../../../stores/useAuthStore";
 import { useForm } from "react-hook-form";
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router';
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router";
 import Modal from "react-modal";
-import InputForm from '../InputFormReset';
+import InputForm from "../InputFormReset";
 const PrivasiContent = () => {
     const { clearToken } = useAuthStore();
     const navigate = useNavigate();
@@ -29,30 +29,30 @@ const PrivasiContent = () => {
     }, [otpTimer]);
     const handleDelete = async () => {
         Swal.fire({
-            title: 'Apakah anda yakin ingin menghapus akun?',
-            text: 'Anda tidak dapat mengembalikan akun ini!',
-            icon: 'warning',
+            title: "Apakah anda yakin ingin menghapus akun?",
+            text: "Anda tidak dapat mengembalikan akun ini!",
+            icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Ya, saya yakin',
-            cancelButtonText: 'Batal'
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Ya, saya yakin",
+            cancelButtonText: "Batal",
         }).then(async (result) => {
             if (result.isConfirmed) {
                 try {
-                    await api.delete('/users');
+                    await api.delete("/users");
                     clearToken();
                     Toast.fire({
-                        icon: 'success',
-                        title: 'Akun berhasil dihapus'
+                        icon: "success",
+                        title: "Akun berhasil dihapus",
                     });
-                    navigate('/login');
+                    navigate("/login");
                 } catch (error) {
                     Toast.fire({
-                        icon: 'error',
-                        title: 'Gagal menghapus akun. Silakan coba lagi.'
+                        icon: "error",
+                        title: "Gagal menghapus akun. Silakan coba lagi.",
                     });
-                    console.error('Error deleting account:', error);
+                    console.error("Error deleting account:", error);
                 }
             }
         });
@@ -67,52 +67,50 @@ const PrivasiContent = () => {
         if (otpTimer > 0) return;
         setOtpTimer(120);
         try {
-            
-            await api.post('/users/update/request-otp');
+            await api.post("/users/update/request-otp");
             Toast.fire({
-                icon: 'success',
-                title: 'OTP berhasil dikirimkan ke email Anda'
+                icon: "success",
+                title: "OTP berhasil dikirimkan ke email Anda",
             });
-            
+
             setIsModalOpen(true);
         } catch (error) {
             Toast.fire({
-                icon: 'error',
-                title: 'Gagal mengirimkan OTP. Silakan coba lagi.'
+                icon: "error",
+                title: "Gagal mengirimkan OTP. Silakan coba lagi.",
             });
         }
     };
 
     const onSubmit = async (data) => {
-        
         const userData = {
             ...data,
         };
         const resetData = {
             old_password: userData.old_password,
             new_password: userData.new_password,
-            otp: userData.otp
-        }
+            otp: userData.otp,
+        };
         console.log(resetData);
         try {
-            await api.put('/users/update/password', resetData);
+            await api.put("/users/update/password", resetData);
             Toast.fire({
-                icon: 'success',
-                title: 'Password berhasil diubah'
+                icon: "success",
+                title: "Password berhasil diubah",
             });
             closeModal();
         } catch (error) {
             if (error.response && error.response.status === 400) {
                 Toast.fire({
-                    icon: 'error',
-                    title: 'Kode Otp Salah'
+                    icon: "error",
+                    title: "Kode Otp Salah",
                 });
             } else {
                 Toast.fire({
-                    icon: 'error',
-                    title: 'Password Lama salah'
+                    icon: "error",
+                    title: "Password Lama salah",
                 });
-                console.error('Error updating password:', error);
+                console.error("Error updating password:", error);
             }
         }
     };
@@ -125,23 +123,20 @@ const PrivasiContent = () => {
         <div className="p-6">
             <h2 className="text-2xl font-bold mb-4">Pengaturan Privasi</h2>
             <hr />
-            <div className='flex flex-row justify-between'>
-                <h2 className='p-5'>Reset Password</h2>
+            <div className="flex flex-row justify-between">
+                <h2 className="p-5">Reset Password</h2>
                 <button
-                    className={`bg-primary m-3 py-3 px-3 block w-[120px] rounded-lg text-white ${otpTimer > 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+                    className={`bg-primary m-3 py-3 px-3 block w-[120px] rounded-lg text-white ${otpTimer > 0 ? "opacity-50 cursor-not-allowed" : ""}`}
                     onClick={handleRequestOTP}
                     disabled={otpTimer > 0}
                 >
-                    {otpTimer > 0 ? `Tunggu (${otpTimer}s)` : 'Reset'}
+                    {otpTimer > 0 ? `Tunggu (${otpTimer}s)` : "Reset"}
                 </button>
             </div>
             <hr />
-            <div className='flex flex-row justify-between'>
-                <h2 className='p-5'>Minta Penghapusan Akun</h2>
-                <button
-                    className='bg-red-500 m-3 py-3 px-3 block w-[120px] rounded-lg text-white'
-                    onClick={handleDelete}
-                >
+            <div className="flex flex-row justify-between">
+                <h2 className="p-5">Minta Penghapusan Akun</h2>
+                <button className="bg-red-500 m-3 py-3 px-3 block w-[120px] rounded-lg text-white" onClick={handleDelete}>
                     Menghapus
                 </button>
             </div>
@@ -154,63 +149,56 @@ const PrivasiContent = () => {
             >
                 <h2 className="text-xl font-bold mb-4">Ubah Password</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                        <InputForm
-                            id="password-label1"
-                            label="Password Lama"
-                            type={showPasswordOld ? "text" : "password"}
-                            register={register("old_password", {
-                                required: "Kata sandi anda tidak valid.",
-                                minLength: {
-                                    value: 6,
-                                    message: "Kata sandi minimal harus 6 karakter",
-                                },
-                            })}
-                            error={errors.old_password?.message}
-                            placeholder="Masukkan password"
-                            showPassword={showPasswordOld}
-                            togglePassword={togglePasswordOld}
-                        />
-                        <InputForm
-                            id="password-label2"
-                            label="Password Baru"
-                            type={showPasswordNew ? "text" : "password"}
-                            register={register("new_password", {
-                                required: "Kata sandi anda tidak valid.",
-                                minLength: {
-                                    value: 6,
-                                    message: "Kata sandi minimal harus 6 karakter",
-                                },
-                            })}
-                            error={errors.new_password?.message}
-                            placeholder="Masukkan password"
-                            showPassword={showPasswordNew}
-                            togglePassword={togglePasswordNew}
-                        />
-                        <label className="block text-sm font-bold mb-1">OTP</label>
-                        <input
-                            type="text"
-                            className="text-center text-3xl font-medium focus:outline-none focus:ring-0 placeholder-gray-400 border border-gray-300 w-full mb-4 py-4 rounded-xl text-primary"
-                            onInput={(e) => {
-                                e.target.value = e.target.value.replace(/[^0-9]/g, "");
-                            }}
-                            maxLength={6}
-                            {...register("otp", {
-                                required: "Silahkan masukkan OTP sebelum lanjut.",
-                            })}
-                        />
-                        {errors.otp && <p className="text-[#EF4444] text-xs mt-2">{errors.otp.message}</p>}
+                    <InputForm
+                        id="password-label1"
+                        label="Password Lama"
+                        type={showPasswordOld ? "text" : "password"}
+                        register={register("old_password", {
+                            required: "Kata sandi anda tidak valid.",
+                            minLength: {
+                                value: 6,
+                                message: "Kata sandi minimal harus 6 karakter",
+                            },
+                        })}
+                        error={errors.old_password?.message}
+                        placeholder="Masukkan password"
+                        showPassword={showPasswordOld}
+                        togglePassword={togglePasswordOld}
+                    />
+                    <InputForm
+                        id="password-label2"
+                        label="Password Baru"
+                        type={showPasswordNew ? "text" : "password"}
+                        register={register("new_password", {
+                            required: "Kata sandi anda tidak valid.",
+                            minLength: {
+                                value: 6,
+                                message: "Kata sandi minimal harus 6 karakter",
+                            },
+                        })}
+                        error={errors.new_password?.message}
+                        placeholder="Masukkan password"
+                        showPassword={showPasswordNew}
+                        togglePassword={togglePasswordNew}
+                    />
+                    <label className="block text-sm font-bold mb-1">OTP</label>
+                    <input
+                        type="text"
+                        className="text-center text-3xl font-medium focus:outline-none focus:ring-0 placeholder-gray-400 border border-gray-300 w-full mb-4 py-4 rounded-xl text-primary"
+                        onInput={(e) => {
+                            e.target.value = e.target.value.replace(/[^0-9]/g, "");
+                        }}
+                        maxLength={6}
+                        {...register("otp", {
+                            required: "Silahkan masukkan OTP sebelum lanjut.",
+                        })}
+                    />
+                    {errors.otp && <p className="text-[#EF4444] text-xs mt-2">{errors.otp.message}</p>}
                     <div className="flex justify-end">
-                        <button
-                            type="button"
-                            onClick={closeModal}
-                            className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 mr-2"
-                        >
+                        <button type="button" onClick={closeModal} className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 mr-2">
                             Batal
                         </button>
-                        <button
-                            type="submit"
-                            className="px-4 py-2 bg-primary text-white rounded hover:bg-primary-dark"
-                        >
+                        <button type="submit" className="px-4 py-2 bg-primary text-white rounded hover:bg-primary-dark">
                             Ubah
                         </button>
                     </div>
