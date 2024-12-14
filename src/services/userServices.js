72360d15c78cf7a1ba01f1f7d358aedece0f11ae
arufaki <fakihkazorla@gmail.com>
2024-12-15 00:25:17 +0700

    refactor(user): updates user service to accept endpoint

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,9 +1,9 @@
 import api from "./api";
 
-const userServices = async () => {
+const userServices = async (endpoint) => {
     try {
-        const response = await api.get("/admin/users");
-        return response.data;
+        const response = await api.get(endpoint);
+        return response.data.data;
     } catch (error) {
         console.error("Error Fetch User Profile", error);
         throw error;
